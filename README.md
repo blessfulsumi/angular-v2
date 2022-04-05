9cc63e3776641b244568812cf534d74d610009d9
blessfulsumi <blessfulsumi@gmail.com>
2022-04-05 14:35:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wzkzwy-acedkm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wzkzwy-acedkm)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+ buy() {
+    window.alert('Buy the best product!');
+  }
+  userReview() {
+    window.alert('Worth for money... You can buy!');
+  }
+  welcome(){
+    window.alert('Thanks for visiting ..Come Again!!!');
+  }
+
+  onNotify(){
+    window.alert('Notification');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
